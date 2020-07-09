720d4918d3c1e53fecd07a2d6e4dadfafee02020
Sohil876 <sohil876@gmail.com>
2020-07-09 15:05:59 +0000

    Change to 2GB heap config

--- msm8953.mk
+++ msm8953.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
